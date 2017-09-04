352c31ebcefb6d483f35e9f92920ac519c0c5240
Iryna <irynkakh@gmail.com>
2017-09-04 11:06:49 +0200

    modified test

--- SumOfTheDigits/src/test/GetsNumbersTest.java
+++ SumOfTheDigits/src/test/GetsNumbersTest.java
@@ -1,24 +1,96 @@
 package test;
 
-import main.GetsNumbers;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
+//import org.junit.platform.commons.meta.API.Usage.Experimental;
+import main.GetsNumbers;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+//import org.junit.platform.runner.JUnitPlatform;
 
 public class GetsNumbersTest {
 
     @Test
+    @Tag("development")
+    @Tag("production")
     public void testSumOfDigits() {
         GetsNumbers getsNumbers = new GetsNumbers();
         int result = getsNumbers.sumOdDigits(917);
-        assertEquals("Sum of digits (917) = ", 17, result);
+        assertEquals( 17, result);
+    }
+    @Test
+    @Tag("development")
+    public void sumOfDigitsNotExceed15Sec(){
+        GetsNumbers getsNumbers = new GetsNumbers();
+        assertTimeout(Duration.ofSeconds(15), () -> {
+            getsNumbers.sumOdDigits(917);
+        });
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
+    @Tag("development")
+    @Tag("production")
+    public void sumOfDigitsExceed1Sec(){
+
+        GetsNumbers getsNumbers = new GetsNumbers();
+        assertTimeout(Duration.ofSeconds(1), () -> {
+            Thread.sleep(2000);
+            getsNumbers.sumOdDigits(999);
+        }, "The sumofDigits method take more than 1 second");
+    }
+    @Test
+    @Tag("development")
     public void testCountOfDigits() {
         GetsNumbers getsNumbers = new GetsNumbers();
-        int result = getsNumbers.sumOdDigits(2222);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> getsNumbers.sumOdDigits(2222));
+
+        // int result = getsNumbers.sumOdDigits(2222);
     }
+
+    @BeforeAll
+    public static void beforeAll_ini() {
+        System.out.println("This will be executed before all methods");
+
+    }
+
+    @AfterAll
+    public static void afterAllMethod(){
+        System.out.println("This will be executed after all methods");
+    }
+
+    @BeforeEach
+    public void beforeEachMethod() {
+        System.out.println("This will be executed before each method");
+    }
+
+    @AfterEach
+    public void afterEachMethod(){
+        System.out.println("This will be executed after each method");
+    }
+
+    @Disabled
+    @Test
+    public void testNotBeExecuted(){
+        System.out.println("This will be ignored");
+    }
+
+     // @ParameterizedTest
+    /*@ValueSource(ints = {15, 30, 150})
+    public void testmethod(int p) throws Exception {
+        GetsNumbers getsNumbers = new GetsNumbers();
+
+        assertEquals("15", getsNumbers.sumOdDigits(p));
+         }*/
+
+   // @RunWith(JUnitPlatform.class)
+   // @IncludeEngines("junit-jupiter")
+   // @SelectPackages("...")
 
 
 }
+
+
