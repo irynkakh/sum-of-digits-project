bcf4a030bd928fbf22fe43aa34f3d5f71a093134
Iryna <irynkakh@gmail.com>
2017-10-09 14:09:20 +0200

    added changes in TestSuite class, fixing in Class3

--- SumOfTheDigits/src/suite/tests/SecondScopeTest.java
+++ SumOfTheDigits/src/suite/tests/SecondScopeTest.java
@@ -1,9 +1,11 @@
 package suite.tests;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SecondScopeTest {
 
+    @Disabled
     @Test
     public void SecondClassFirstMethod() {
         System.out.println("Second class, first method");
--- SumOfTheDigits/src/test/Class3.java
+++ SumOfTheDigits/src/test/Class3.java
@@ -1,37 +1,23 @@
 package test;
 
 import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-
-
-@RunWith(JUnitPlatform.class)
-@SelectPackages("test")
-@IncludeTags("development")
 public class Class3 extends TestBase {
 
-
-    @Tag("production")
     @Test
     public void firstTest() {
         System.out.println("first test");
     }
 
-    @Tag("development")
+
     @Test
     public void secondTest() {
         System.out.println("second test");
     }
 
-    @Tag("development")
     @Test
     public void thirdTest() {
         System.out.println("third test");
